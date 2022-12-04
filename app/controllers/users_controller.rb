1652cfcd47216fd3b0ddc1d5009a4bf6c8a35158
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-04 04:41:48 +0300

    add  the index, show and create models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index
+        @users = User.all 
+        render json: @users, status: 200
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user
+    end
+    
+
+    private 
+    def user_params
+        params.permit(:name,:username, :address, :email, :phone_number)
+    end
+
+    def render_not_found_response
+        render json: {error: ["User Not Found"]}, status: :not_found
+    end
+
+    
+    
 end
