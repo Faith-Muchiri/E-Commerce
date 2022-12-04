f6229b874c7bbe3668aedb71f3e47216a5f053ef
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-04 07:04:48 +0300

    modify user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
     def index
         @users = User.all 
         render json: @users, status: 200
@@ -25,6 +27,8 @@ class UsersController < ApplicationController
         render json: {error: ["User Not Found"]}, status: :not_found
     end
 
-    
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_one :cart
     has_many :reviews
+    has_many :products, through: :reviews
     validates :name, :username, :email, :phone_number, presence: true, uniqueness: true
     validates :address, presence: true
 end
