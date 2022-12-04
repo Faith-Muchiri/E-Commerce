411094325963b147a6b70732f9e2a653236c1d3e
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-04 05:00:15 +0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_one :cart
     has_many :reviews
+    validates :name, :username, :email, :phone_number, presence: true, uniqueness: true
+    validates :address, presence: true
 end
