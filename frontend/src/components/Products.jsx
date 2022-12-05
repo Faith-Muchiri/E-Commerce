2777ad2756b4d73e92a2b53a215231cc4d4ad74a
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-05 08:16:05 +0300

    fetch products

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect}  from 'react'
 
 function Products() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('/products')
+    .then(response => response.json())
+    .then((data) => setProducts(data) )
+  },[])
+
+  
   return (
     <div>
-      
+     {products.map((product) => (
+      <div key={product.id}>
+        <img src={product.image_url} alt={product.name}/>
+      </div>
+     ))}
     </div>
   )
 }
