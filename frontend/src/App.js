654b85be97de3f951f824702aae82dd6fccc197d
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-05 19:37:54 +0300

    modify app js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './components/Home';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Products from './components/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>    
+      <div className="App">
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/logout" element={<Logout/>}/>
+          <Route path="/products" element={<Products/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
