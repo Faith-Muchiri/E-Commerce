3154a41cfd5741d1f84d256375ec62b0339bbed9
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-05 06:02:59 +0300

    add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
 import Home from './components/Home';
 import Login from './components/Login';
 import Logout from './components/Logout';
@@ -7,17 +8,17 @@ import Products from './components/Products';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter>    
       <div className="App">
+        <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/logout" element={<Logout/>}/>
           <Route path="/products" element={<Products/>}/>
         </Routes>
-    </div>
+      </div>
     </BrowserRouter>
-
   );
 }
 
